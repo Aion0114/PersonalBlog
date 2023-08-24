7724ff05572034d9e2d895be1b481090bc287ac4
aion <tttt303384979@163.com>
2023-08-24 14:22:34 +0800

    完善我的项目模块，调整ui

--- personal-blog-frontend/src/views/projectView.vue
+++ personal-blog-frontend/src/views/projectView.vue
@@ -11,60 +11,22 @@
             <div class="project-list-item">
               <a title="" target="_blank">
                 <div class="project-list-item-icon">
-
+                  <img src="../assets/Pokecoins.png" alt="个人博客">
                 </div>
                 <div class="project-list-item-info">
                   <div class="project-list-item-name">个人博客</div>
+                  <div class="project-list-item-detail">每个人心中应该都有属于自己展示的舞台吧!</div>
                 </div>
               </a>
             </div>
             <div class="project-list-item">
               <a title="" target="_blank">
                 <div class="project-list-item-icon">
-
+                  <img src="../assets/imageDecry.png" alt="个人博客">
                 </div>
                 <div class="project-list-item-info">
                   <div class="project-list-item-name">基于二维码为载体的图像加密管理系统</div>
-                </div>
-              </a>
-            </div>
-            <div class="project-list-item">
-              <a title="" target="_blank">
-                <div class="project-list-item-icon">
-
-                </div>
-                <div class="project-list-item-info">
-                  <div class="project-list-item-name">项目3</div>
-                </div>
-              </a>
-            </div>
-            <div class="project-list-item">
-              <a title="" target="_blank">
-                <div class="project-list-item-icon">
-
-                </div>
-                <div class="project-list-item-info">
-                  <div class="project-list-item-name">项目4</div>
-                </div>
-              </a>
-            </div>
-            <div class="project-list-item">
-              <a title="" target="_blank">
-                <div class="project-list-item-icon">
-
-                </div>
-                <div class="project-list-item-info">
-                  <div class="project-list-item-name">项目5</div>
-                </div>
-              </a>
-            </div>
-            <div class="project-list-item">
-              <a title="" target="_blank">
-                <div class="project-list-item-icon">
-
-                </div>
-                <div class="project-list-item-info">
-                  <div class="project-list-item-name">项目6</div>
+                  <div class="project-list-item-detail">我的毕业设计!</div>
                 </div>
               </a>
             </div>
@@ -140,8 +102,8 @@ export default {
   float: left;
   overflow: hidden;
   line-height: 17px;
-  height: 200px;
-  padding: 10px;
+  height: 150px;
+  padding: 20px;
   border-radius: 8px;
   border: 1px solid #e3e8f7;
   transition: all .3s ease-in-out;
@@ -156,5 +118,76 @@ export default {
   border-color: #425aef!important;
   background-color: #425aef!important;
   box-shadow: 0 2px 12px -3px #425aef!important;
+}
+
+.project-list-item-icon {
+  float: left;
+  width: 100px;
+  height: 100px;
+  transition: all .3s ease-out;
+  margin: 5px 0 20px 0;
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.project-list-item-icon img {
+  display: block;
+  margin: 0 auto 20px;
+  max-width: 100%;
+  transition: filter 375ms ease-in .2s;
+  object-fit: cover;
+  max-height: 900px;
+  width: 100%;
+  height: 100%;
+  border-style: none;
+}
+
+.project-list-item:hover .project-list-item-icon {
+  width: 0;
+  height: 0;
+  margin-left: -10px;
+}
+
+.project-list-item-info {
+  display: flex;
+  flex-wrap: wrap;
+  padding-left: 20px;
+  padding-top: 15px;
+  text-align: left;
+  flex-direction: column;
+}
+
+.project-list-item-name {
+  padding: 12px 0 16px 0;
+  height: auto;
+  line-height: 1;
+  font-weight: 700;
+  font-size: 1.1rem;
+  color: #363636;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  /*white-space: nowrap;*/
+}
+
+.project-list-item:hover .project-list-item-name {
+  color: #fff;
+}
+
+.project-list-item-detail {
+  padding: 0;
+  height: 35px;
+  font-size: .93rem;
+  opacity: .7;
+  color: #363636;
+  word-break: break-all;
+  white-space: break-spaces;
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+}
+
+.project-list-item:hover .project-list-item-detail {
+  color: #fff;
 }
 </style>
